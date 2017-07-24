45e558b09545edd3f9cae8cb943a1d3f29defae2
Cuiqing Li <lixx3527@gmail.com>
2017-07-24 18:40:00 -0400

    pid controller codes for python

--- PID_controller_examples/PID_controller.py
+++ PID_controller_examples/PID_controller.py
@@ -2,15 +2,18 @@
 # User Instructions
 #
 # Implement a P controller by running 100 iterations
-# of robot motion. The desired trajectory for the 
-# robot is the x-axis. The steering angle should be set
+# of robot motion. The steering angle should be set
 # by the parameter tau so that:
 #
-# steering = -tau * crosstrack_error
+# steering = -tau_p * CTE - tau_d * diff_CTE - tau_i * int_CTE
 #
-# You'll only need to modify the `run` function at the bottom.
+# where the integrated crosstrack error (int_CTE) is
+# the sum of all the previous crosstrack errors.
+# This term works to cancel out steering drift.
+#
+# Only modify code at the bottom! Look for the TODO.
 # ------------
- 
+
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -95,27 +98,21 @@ class Robot(object):
     def __repr__(self):
         return '[x=%.5f y=%.5f orient=%.5f]' % (self.x, self.y, self.orientation)
 
-############## ADD / MODIFY CODE BELOW ####################
-# ------------------------------------------------------------------------
-#
-# run - does a single control run
+
 robot = Robot()
 robot.set(0, 1, 0)
 
-def run(robot, tau, n=100, speed=1.0):
+
+def run(robot, tau_p, tau_d, tau_i, n=100, speed=1.0):
     x_trajectory = []
     y_trajectory = []
-    for i in range(n):
-        cte = robot.y
-        steer = -tau * cte
-        robot.move(steer, speed)
-        x_trajectory.append(robot.x)
-        y_trajectory.append(robot.y)
+    # TODO: your code here
     return x_trajectory, y_trajectory
-    
-x_trajectory, y_trajectory = run(robot, 0.1)
+
+
+x_trajectory, y_trajectory = run(robot, 0.2, 3.0, 0.004)
 n = len(x_trajectory)
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
-ax1.plot(x_trajectory, y_trajectory, 'g', label='P controller')
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8,8))
+ax1.plot(x_trajectory, y_trajectory, 'g', label='PID controller')
 ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
